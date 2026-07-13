ffc4bb25b128aaa9573d64db53c1f458b667bef9
Debadrito Pal <debadrito123@gmail.com>
2026-07-13 19:39:24 +0530

    Fibonacci Series Generator

---
+++ Recursion/Fibonacci.py
@@ -0,0 +1,24 @@
+def Fibonacci(num):
+    if num==0:
+        return 0
+    elif num==1:
+        return 1
+    return Fibonacci(num-1)+Fibonacci(num-2)
+def printFib(num):
+    l=[]
+    for fib in range(num+1):
+        l.append(Fibonacci(fib))
+    return l
+while True:
+    try:
+        num=int(input("Enter a positive integer (Enter -1 to exit): "))
+        if num>=0:
+            fib_seq=printFib(num)
+            print(f"Fibonacci series till {num}: ",*fib_seq)
+        elif(num==-1):
+            print("Exiting....")
+            break
+        else:
+            print("Please enter a positive integer")
+    except ValueError:
+        print("Please enter a valid positive integer")
